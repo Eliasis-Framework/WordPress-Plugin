c9614cb09d390022e5b68163d98e353531e46ae1
Josantonius <info@josantonius.com>
2017-03-29 10:29:19 +0200

    Initial upload

--- config/menu.php
+++ config/menu.php
@@ -17,20 +17,20 @@ return [
 
 	'menu' => [
 		'top-level' => [
-			'title'      => __('Eliasis WP Plugin', $textdomain),
-			'name'       => __('Eliasis WP Plugin', $textdomain),
+			'title'      => __('Eliasis WP Plugin', 'eliasis-wordpress-plugin'),
+			'name'       => __('Eliasis WP Plugin', 'eliasis-wordpress-plugin'),
 			'capability' => 'manage_options',
 			'slug'       => 'eliasis-wp-options',
 			'function'   => '',
 			'icon_url'   => App::url('icons') . 'eliasis-wp-menu-admin.png',
-			'position'   => 25,
+			'position'   => 26,
 		],
 	],
 	'submenu' => [
 		'options' => [
 			'parent'     => 'eliasis-wp-options',
-			'title'      => __('Options', $textdomain),
-			'name'       => __('Options', $textdomain),
+			'title'      => __('Options', 'eliasis-wordpress-plugin'),
+			'name'       => __('Options', 'eliasis-wordpress-plugin'),
 			'capability' => 'manage_options',
 			'slug'       => 'eliasis-wp-options',
 			'function'   => '',
